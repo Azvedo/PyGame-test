71f7c300f20a8bc4186660584ee251f895490b7c
Rafael <raas@cin.ufpe.br>
2024-02-20 22:36:47 -0300

    first commit

---
+++ car.py
@@ -0,0 +1,9 @@
+ # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_LEFT: #verifica qual tecla foi pressionada
+        #         main_game.car.position.x -= 40
+        #     if event.key == pygame.K_RIGHT: #verifica qual tecla foi pressionada
+        #         main_game.car.position.x += 40 
+        #     if event.key == pygame.K_UP:
+        #         main_game.car.position.y -=10
+        #     if event.key == pygame.K_DOWN:
+        #         main_game.car.position.y +=10
